95cf67fa99915f1853d166070b867f9ad70bbe05
wangzhazha123 <243233110@qq.com>
2017-07-25 09:45:35 +0800

    Add files via upload

---
+++ task-14/outer.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 王渣渣 on 2017/7/22.
+ */
+var s=document.getElementById("target-ul");
+
+function showUl() {
+    if(s.style.display==""){
+        s.style.display="block";
+    }else{
+        s.style.display="";
+    }
+}
